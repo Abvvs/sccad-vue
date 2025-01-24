f8a434b1053391531bad74932c7fdcfa3b86c97b
Abvvs <abisanchez143@gmail.com>
2025-01-24 17:14:48 -0500

    hero component, arreglar banner

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-
+<section class="relative w-full h-[50%]">
+    <!-- Imagen de fondo -->
+    <div class="absolute inset-0 top-0">
+        <img
+            src="../assets/banner_home.jpg"
+            alt="Imagen de fondo"
+            class="w-full h-full object-cover"
+        />
     </div>
+
+    <!-- Texto sobre la imagen -->
+    <div
+      class="absolute inset-0 flex items-center justify-center "
+    >
+      <h1 class="text-white text-3xl md:text-5xl font-bold">
+        SCCAD Topografía
+      </h1>
+    </div>
+  </section>
 </template>
 
-<script>
-    export default {
-        
-    }
-</script>
+<script setup></script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
